1684dbfde9b71172701af1b7aed4ef5e37a9c163
ivano panizza <ivano.panizza@libero.it>
2019-03-05 15:55:10 +0100

    all funcionalities implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,42 @@
 class TasksController < ApplicationController
   def index         # GET /tasks
+    @tasks = Task.all
   end
 
   def show          # GET /tasks/:id
+    @task = Task.find(params[:id])
   end
 
   def new           # GET /tasks/new
+    @task = Task.new
   end
 
   def create        # POST /tasks
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
 
   def edit          # GET /tasks/:id/edit
+    @task = Task.find(params[:id])
   end
 
   def update        # PATCH /tasks/:id
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
   end
 
   def destroy       # DELETE /tasks/:id
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
